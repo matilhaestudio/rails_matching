d877346aa84c7b8bc5038433f9563bcb4db98be7
Jonas Scherer <jonas@matilhaestud.io>
2017-05-21 17:28:53 -0300

    first test for model instances

--- spec/rails_matching_spec.rb
+++ spec/rails_matching_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe RailsMatching do
   	expect(user).to be_instance_of(User)
   end
 
+  it "creates a list of users to run tests" do
+  	user = create_list(:user, 10)
+  	expect(user.count).to be(10)
+  end
+
 end
