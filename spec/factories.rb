7a19d86b93b4e6485262148aa4e16ed632a94968
Jonas Scherer <jonas@matilhaestud.io>
2017-05-21 17:27:04 -0300

    first test for model instances

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-# spec/factories.rb
-# FactoryGirl.define do
-#   factory :user do
-#     name "Jonas"
-#     age 22
-#     job_title "CEO"
-#   end
-# end
---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+# spec/factories.rb
+FactoryGirl.define do
+  factory :user do
+    name "Jonas"
+    age 22
+    job_title "CEO"
+  end
+end
