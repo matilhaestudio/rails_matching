25aea31f13cdc38b01df38d15200c9ad41cbdffa
Jonas Scherer <jonas@matilhaestud.io>
2017-05-22 16:07:39 -0300

    specs

--- spec/rails_matching_spec.rb
+++ spec/rails_matching_spec.rb
@@ -2,26 +2,39 @@ require "spec_helper"
 
 RSpec.describe RailsMatching do
 
-  it "has a version number" do
-    expect(RailsMatching::VERSION).not_to be nil
-  end
- 
-  it "creates a user to run tests" do
-  	user = create(:user)
-  	expect(user).to be_instance_of(User)
+  context "Setup"
+
+    it "has a version number" do
+      expect(RailsMatching::VERSION).not_to be nil
+    end
+   
+    it "creates a user to run tests" do
+    	user = create(:user)
+    	expect(user).to be_instance_of(User)
+    end
+
+    it "creates a list of users to run tests" do
+    	users = create_list(:user, 10)
+    	expect(users.count).to be(10)
+    	# pp users
+    end
   end
 
-  it "creates a list of users to run tests" do
-  	users = create_list(:user, 10)
-  	expect(users.count).to be(10)
-  	# pp users
+  context "Model against itself matches"
+
+    it "Raise an error if first param is not an object" do
+    end
+
+    it "Raise and error if first params does not have an attibute"
+    end
+
+    it "runs matches" do
+    	# 10.times {
+    	# 	user = create(:user)	
+    	# }
+     #  matchs = RailsMatching.against_itself(User.all)
+     #  pp User.all
+    end
   end
 
-  it "runs matches" do
-  	# 10.times {
-  	# 	user = create(:user)	
-  	# }
-   #  matchs = RailsMatching.against_itself(User.all)
-   #  pp User.all
-  end
 end
